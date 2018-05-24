0edc068f793870aa71f187de0123bdff8c6dc0f9
ferhat.sahinkaya <ferhat.sahinkaya@sky.uk>
2018-05-24 23:50:51 +0100

    Start using hbc client to use twitter streaming api

--- src/main/java/com/innovation/tweetreader/Main.java
+++ src/main/java/com/innovation/tweetreader/Main.java
@@ -1,12 +1,49 @@
 package com.innovation.tweetreader;
 
-import com.innovation.tweetreader.twitter.BearerTokenRetriever.BearerToken;
+import com.innovation.tweetreader.config.domain.Credentials;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
 
-import static com.innovation.tweetreader.twitter.BearerTokenRetriever.bearerTokenRetriever;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.innovation.tweetreader.config.reader.JsonConfigReader.readConfig;
+import static com.twitter.hbc.core.Constants.STREAM_HOST;
 
 public class Main {
     public static void main(String[] args) {
-        final BearerToken bearerToken = bearerTokenRetriever().bearerToken();
-        System.out.println("BearerToken : " + bearerToken.tokenType() + ", AccessToken : " + bearerToken.accessToken());
+        BlockingQueue<String> queue = new LinkedBlockingQueue<>(100000);
+
+        Credentials credentials = readConfig("credentials.json", Credentials.class);
+
+        Authentication authentication = new OAuth1(
+                credentials.consumerKey(),
+                credentials.consumerSecret(),
+                credentials.token(),
+                credentials.tokenSecret());
+
+        Client client = new ClientBuilder()
+                .name("tweet-reader-client")
+                .hosts(STREAM_HOST)
+                .authentication(authentication)
+                .endpoint(new StatusesFilterEndpoint().trackTerms(newArrayList("twitter", "api")))
+                .processor(new StringDelimitedProcessor(queue))
+                .build();
+
+        client.connect();
+
+        while (!client.isDone()) {
+            try {
+                System.out.println(queue.take());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        client.stop();
     }
 }
--- src/main/java/com/innovation/tweetreader/config/domain/Credentials.java
+++ src/main/java/com/innovation/tweetreader/config/domain/Credentials.java
@@ -9,11 +9,25 @@ public class Credentials {
     @SerializedName("consumer_secret")
     String consumerSecret;
 
+    @SerializedName("token")
+    String token;
+
+    @SerializedName("token_secret")
+    String tokenSecret;
+
     public String consumerKey() {
         return consumerKey;
     }
 
     public String consumerSecret() {
         return consumerSecret;
+    }
+
+    public String token() {
+        return token;
+    }
+
+    public String tokenSecret() {
+        return tokenSecret;
     }
 }
